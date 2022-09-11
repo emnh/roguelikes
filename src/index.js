75d3f61f997fd002461c378d38ca00287760fe92
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2022-09-11 11:17:01 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,58 +1,46 @@
-function loadImage(size, container, r, scale) {
+function loadImage(r, scale) {
   const src = images[r].replace('dcss', 'dcss' + scale);
-  const imgstr = '<img style="margin-bottom: 5px; border: 1px solid black;" class="main visible" src="' + src + '"></img>';
-  const img2 = container.append(imgstr);
-  const img = $(".main");
-//  img.css('width', '512px');
-//  img.css('height', '512px');
-  //const size2 = Math.floor(Math.min(window.innerWidth, window.innerHeight) * 0.5) - 5;
-//  img.css('width', size + 'px');
-//  img.css('height', size + 'px');
-  //img[0].width = window.innerWidth - 5;
-  //img[0].height = window.innerHeight - 5;
+  const img = $('<img style="margin-bottom: 5px; border: 1px solid black;" class="main visible" src="' + src + '"></img>');
 	return img;
 }
 
 function lis(container2) {
-  const size2 = Math.floor(Math.max(window.innerWidth, window.innerHeight) * 0.5) - 5;
-  const size = Math.min(256, size2);
   const container = $("<div class='page'/>");
   container2.append(container);
   for (let i = 0; i < 1; i++) {
     const r1 = Math.floor(Math.random() * images.length);
     const r2 = Math.floor(Math.random() * images.length);
-    //for (let j = 0; j < 3; j++) {
-//    container.append("<h3>Swipe best image</h3>");
 		const div1 = $("<div class='ic top' />");
-		//const arrow1 = "<h1 class='arrow left'>←</h1>";
-		const arrow1 = $("<img src='images/right-arrow.svg' class='arrow up'></img>");
-		const like1 = $("<img src='images/like.svg' class='like up'></img>");
-//    arrow1[0].style.width = size + 'px';
-    //arrow1[0].style.height = Math.floor(0.1 * size) + 'px';
+		const arrow1 = $("<img src='images/right-arrow-cropped.svg' class='arrow up'></img>");
+		const like1 = $("<img src='images/like-cropped.svg' class='like up'></img>");
 		container.append(div1);
     div1.append(like1);
     div1.append(arrow1);
-		const img1 = loadImage(size, div1, r1, '16x');
+		const img1 = loadImage(r1, '16x');
+    div1.append(img1);
 
 		const div2 = $("<div class='ic bottom' />");
-		//const arrow2 = "<h1 class='arrow right'>→</h1>";
-		const arrow2 = $("<img src='images/right-arrow.svg' class='arrow down'></img>");
-		const like2 = $("<img src='images/like.svg' class='like down'></img>");
-//    arrow2[0].style.width = size + 'px';
-    //arrow2[0].style.height = Math.floor(0.1 * size) + 'px';
+		const arrow2 = $("<img src='images/right-arrow-cropped.svg' class='arrow down'></img>");
+		const like2 = $("<img src='images/like-cropped.svg' class='like down'></img>");
 		container.append(div2);
-    const img2 = loadImage(size, div2, r2, '16x');
+    const img2 = loadImage(r2, '16x');
+    div2.append(img2);
 		div2.append(arrow2);
 		div2.append(like2);
-//    for (let j = 0; j < 1; j++) {
-//      const scale =
-//        j === 0 ?
-//          '16x' :
-//            (j === 1 ? '4x' : '');
-//      loadImage(r, scale);
-//  }
-//    $("body").append('<br/>');
   }
+}
+
+function relis(container2) {
+    const r1 = Math.floor(Math.random() * images.length);
+    const r2 = Math.floor(Math.random() * images.length);
+		const ctop = container2.find(".ic.top");
+    const cbottom = container2.find(".ic.bottom");
+    ctop.find("img.main").remove();
+    cbottom.find("img.main").remove();
+		const img1 = loadImage(r1, '16x');
+    ctop.append(img1);
+    const img2 = loadImage(r2, '16x');
+    cbottom.prepend(img2);
 }
 
 function main() {
@@ -85,8 +73,8 @@ function main() {
 
    		on('slideChangeTransitionEnd', () => {
         const container = $(swiper.slides[swiper.previousIndex]); // containers[(swiper.previousIndex + maxi - 1) % maxi];
-        container.empty();
-        lis(container);
+        //container.empty();
+        relis(container);
 				console.log('slideChangeTransitionEnd', swiper.previousIndex, '->', swiper.activeIndex);
       });
 //       on('slideNextTransitionEnd', () => {
@@ -117,7 +105,14 @@ function main() {
   const swiper = new Swiper(".mySwiper", {
 		modules: [myPlugin],
 		direction: "vertical",
-		loop: true
+		loop: true,
+    navigation: {
+      nextEl: ".up",
+      prevEl: ".down",
+    },
+    keyboard: {
+      enabled: true,
+    }
 	});
   console.log(swiper);
   //setInterval(lis, 2000);
