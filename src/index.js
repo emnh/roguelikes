907041bef54a1605a82940acce24f52fcd65f62a
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2022-09-11 08:46:10 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-function loadImage(container, r, scale) {
+function loadImage(size, container, r, scale) {
   const src = images[r].replace('dcss', 'dcss' + scale);
   const imgstr = '<img style="margin-bottom: 5px; border: 1px solid black;" class="main" src="' + src + '"></img>';
   const img2 = container.append(imgstr);
@@ -6,8 +6,6 @@ function loadImage(container, r, scale) {
 //  img.css('width', '512px');
 //  img.css('height', '512px');
   //const size2 = Math.floor(Math.min(window.innerWidth, window.innerHeight) * 0.5) - 5;
-  const size2 = Math.floor(Math.max(window.innerWidth, window.innerHeight) * 0.5) - 5;
-  const size = Math.min(256, size2);
   img.css('width', size + 'px');
   img.css('height', size + 'px');
   //img[0].width = window.innerWidth - 5;
@@ -15,24 +13,31 @@ function loadImage(container, r, scale) {
 	return img;
 }
 
-function lis() {
-  $("body").empty();
+function lis(container) {
+  const size2 = Math.floor(Math.max(window.innerWidth, window.innerHeight) * 0.5) - 5;
+  const size = Math.min(256, size2);
   for (let i = 0; i < 1; i++) {
     const r1 = Math.floor(Math.random() * images.length);
     const r2 = Math.floor(Math.random() * images.length);
     //for (let j = 0; j < 3; j++) {
-		$("body").append("<h1>Swipe left/right</h1>");
+		container.append("<h3>Swipe best image</h3>");
 		const div1 = $("<div class='ic' style='width: 100%;' />");
-		const arrow1 = "<h1 class='arrow left'>←</h1>";
-		$("body").append(div1);
-		const img1 = loadImage(div1, r1, '16x');
-		img1.before(arrow1);
+		//const arrow1 = "<h1 class='arrow left'>←</h1>";
+		const arrow1 = $("<img src='images/right-arrow.svg' class='arrow left'></img>");
+    arrow1[0].style.width = size + 'px';
+    arrow1[0].style.height = Math.floor(0.25 * size) + 'px';
+		container.append(div1);
+		const img1 = loadImage(size, div1, r1, '16x');
+    container.append(arrow1);
 
 		const div2 = $("<div class='ic'/>");
-		const arrow2 = "<h1 class='arrow right'>→</h1>";
-		$("body").append(div2);
-    const img2 = loadImage(div2, r2, '16x');
-		img2.after(arrow2);
+		//const arrow2 = "<h1 class='arrow right'>→</h1>";
+		const arrow2 = $("<img src='images/right-arrow.svg' class='arrow right'></img>");
+    arrow2[0].style.width = size + 'px';
+    arrow2[0].style.height = Math.floor(0.25 * size) + 'px';
+		container.append(div2);
+    const img2 = loadImage(size, div2, r2, '16x');
+		container.append(arrow2);
 //    for (let j = 0; j < 1; j++) {
 //      const scale =
 //        j === 0 ?
@@ -49,9 +54,21 @@ function main() {
   //loadImage();
   //$("body")
   const body = $("body");
-  body.css('width', '100%');
-  body.css('height', '100%');
-  body.css('overflow', 'hidden');
-  setInterval(lis, 2000);
+  body.empty();
+//  body.css('width', '100%');
+//  body.css('height', '100%');
+//  body.css('overflow', 'hidden');
+  const maindiv = $("<div class='swiper mySwiper' />");
+  const wrapper = $("<div class='slider-wrapper' />");
+  body.append(maindiv);
+  maindiv.append(wrapper);
+  for (let i = 0; i < 3; i++) {
+    const container = $("<div class='swiper-slide' />");
+    wrapper.append(container);
+    lis(container);
+  }
+  const swiper = new Swiper(".mySwiper", {});
+  console.log(swiper);
+  //setInterval(lis, 2000);
 }
 $(main);
